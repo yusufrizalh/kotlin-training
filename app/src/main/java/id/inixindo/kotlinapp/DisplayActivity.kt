764223380a3f4b7a292fb6cae9d4417cf54f2ae3
Yusuf Rizal <yusufrizalh@gmail.com>
2022-07-26 15:41:47 +0700

    kotlin training day:1 - actionbar backbutton

--- app/src/main/java/id/inixindo/kotlinapp/DisplayActivity.kt
+++ app/src/main/java/id/inixindo/kotlinapp/DisplayActivity.kt
@@ -1,5 +1,6 @@
 package id.inixindo.kotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -29,16 +30,23 @@ class DisplayActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId){
-//            android.R.id.home -> {
-//                onBackPressed()
-//                return true
-//            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-//
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    //
 //    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 //        menuInflater.inflate(R.menu.main, menu)
 //        return super.onCreateOptionsMenu(menu)
